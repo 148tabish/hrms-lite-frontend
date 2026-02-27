843b3a8b4389848a32fe516b108e26d3bd3f600c
Tabish Khan <91627183+148tabish@users.noreply.github.com>
2026-02-27 20:44:02 +0530

    Update axiosInstance.js

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://hrms-lite-backend-d0jv.onrender.com',
   headers: { 'Content-Type': 'application/json' },
   timeout: 10000,
 })
